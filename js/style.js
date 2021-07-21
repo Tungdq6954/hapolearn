61bdc16389af66d1ac21ade65ca88461242c8ad0
Tungdq6954 <tung.dq@haposoft.com>
2021-07-21 14:35:55 +0700

    finished issue-6

---
+++ js/style.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    $('.feedback-slide').slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        prevArrow: '<button class="slide-arrow prev-arrow"><i class="fas fa-angle-left"></i></button>',
+        nextArrow: '<button class="slide-arrow next-arrow"><i class="fas fa-angle-right"></i></button>',
+        responsive: [
+            {
+              breakpoint: 769,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true
+              }
+            },
+            {
+              breakpoint: 414,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                infinite: true,
+              }
+            }
+          ]
+    });
+});
