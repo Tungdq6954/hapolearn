dfc9b72f382a40c9286313cb34e66c08bca48ac9
Tungdq6954 <tung.dq@haposoft.com>
2021-07-27 22:54:45 +0700

    fix font

--- js/style.js
+++ js/style.js
@@ -42,9 +42,19 @@ $(document).ready(function () {
     }
   });
 
-  $('.navbar-light .navbar-nav .nav-link').on('click', function(){
+  $('.navbar-light .navbar-nav .nav-link').on('click', function () {
     $('.navbar-light .navbar-nav .nav-link').removeClass('active');
     $(this).addClass('active');
+
+    if ($('.custom-navbar-collapse').hasClass('show')) {
+      $('.custom-navbar-collapse').removeClass('show')
+      $('.custom-button-navbar-toggler').removeClass('collapsed');
+      $('.custom-button-navbar-toggler .fas.fa-bars').removeClass('hidden');
+      $('.custom-button-navbar-toggler .fas.fa-times').addClass('hidden');
+      $('.addition-class').addClass('hidden');
+      $('#body').removeClass('hidden');
+      $('footer').removeClass('hidden');
+    }
   });
 
   $('.logo-messenger').on('click', function () {
@@ -55,16 +65,16 @@ $(document).ready(function () {
     $('.chatbox').removeClass('active');
   })
 
-  $('#login-nav-tab').on('click', function(){
-    if($('#register-nav-tab').hasClass('active-tab')) {
+  $('#login-nav-tab').on('click', function () {
+    if ($('#register-nav-tab').hasClass('active-tab')) {
       $('#register-nav-tab').removeClass('active-tab');
     }
 
     $(this).addClass('active-tab');
   });
 
-  $('#register-nav-tab').on('click', function(){
-    if($('#login-nav-tab').hasClass('active-tab')) {
+  $('#register-nav-tab').on('click', function () {
+    if ($('#login-nav-tab').hasClass('active-tab')) {
       $('#login-nav-tab').removeClass('active-tab');
     }
 
